ef96bc39c0039d84c5e148192f5a555efb8627ad
Andrew Fisher <andrewfishersb@gmail.com>
2020-03-24 15:49:07 -0700

    Move code to one file, set up communication with the console

--- URL_Generator.py
+++ URL_Generator.py
@@ -1,5 +1,9 @@
 # https://www.codevoila.com/post/45/open-web-browser-window-or-new-tab-with-python
 # https://www.geeksforgeeks.org/reading-writing-text-files-python/
+
+import webbrowser
+
+
 class URL_Generator:
 
     def __init__(self):
@@ -30,17 +34,66 @@ class URL_Generator:
 
         return self.complete_url
 
+    def open_a_file(self, filename):
+        file = open(filename)
+        initial_film_list = file.readlines()
+        list_of_films = []
+        for x in range(len(initial_film_list)):
+            cur_film = initial_film_list[x]
+            substring_film = cur_film[0:len(cur_film) - 1]
+            list_of_films.insert(x, substring_film)
+            # print(list_of_films[x])
 
+        file.close()
+        return list_of_films
+
+    def write_to_file(self, filename, list_to_write):
+        file = open(filename,"w")
+        for url in list_to_write:
+            file.write(url + "\n")
+        file.close()
 
 if __name__ == '__main__':
 
-    current_film = URL_Generator()
+    generator = URL_Generator()
+    # Collect URLS
+    # OPEN Links
+    movie_list = generator.open_a_file("Movies")
 
-    movie_list = current_film.open_a_file("Movies")
-    for film in movie_list:
-        print(current_film.create_url(film, "Amazon"))
-        # print(current_film.create_url(film, "Netflix"))
-        # print(current_film.create_url(film, "Xfinity"))
+    initial_command = input("What would you like to do (type the number)\n1. Update Links\n2. Open Links\n3. Exit\n")
 
-    current_film.write_to_file("Urls_Amazon_Films")
+    while not initial_command.isdigit() or int(initial_command) != 3:
+        if not initial_command.isdigit() or int(initial_command) <1 or int(initial_command) >2:
+            initial_command = input("What would you like to do (type the number)\n1. Update Links\n2. Open Links\n3. Exit\n")
+        else:
+
+            platform = input("What platform are you using?\n")
+            while platform not in ['Amazon', 'Netflix', 'Xfinity']:
+                platform = input("What platform are you using? (Amazon, Netflix or Xfinity)\n")
+
+            if initial_command == 1:
+                # Write to file
+                url_list = []
+                for film in movie_list:
+                    url = generator.create_url(film, platform)
+                    url_list.append(url)
+
+                if platform == "Amazon":
+                    file_name = "Urls_Amazon_Films"
+                elif platform == "Netflix":
+                    file_name = "Urls_Netflix_Films"
+                else:
+                    file_name = "Urls_Xfinity_Films"
+
+                generator.write_to_file(file_name, url_list)
+            else:
+
+
+            initial_command = input("What would you like to do (type the number)\n1. Update Links\n2. Open Links\n3. Exit\n")
+
+
+
+
+
+
 
